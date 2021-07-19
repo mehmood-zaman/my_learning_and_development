037c76deb77d3ece55895790b45279dc7f4511dc
mehmood-zaman <mehmood497@gmail.com>
2021-07-19 12:59:30 +0500

    updated

--- TS/sorts/build/index.js
+++ TS/sorts/build/index.js
@@ -3,8 +3,19 @@ var Sorter = /** @class */ (function () {
     function Sorter(collection) {
         this.collection = collection;
     }
-    Sorter.prototype.sort = function () { };
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
     return Sorter;
 }());
-var sorter = new Sorter([0, -2, 3, 5]);
-sorter.sort();
+var sorter = new Sorter([0, 3, -3, 5]);
+console.log(sorter.collection);
