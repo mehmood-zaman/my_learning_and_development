97a67a564c9fab08704e58d933bafd915e56a910
mehmood-zaman <mehmood497@gmail.com>
2021-07-27 08:53:43 +0500

    updated

--- TS/sorts/src/index.ts
+++ TS/sorts/src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([0, 3, -3, 5]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([0, 3, -3, 5]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Xaayb");
+const sorter = new Sorter(charactersCollection);
+
 sorter.sort();
-console.log(numbersCollection.data);
+
+console.log(charactersCollection.data);
