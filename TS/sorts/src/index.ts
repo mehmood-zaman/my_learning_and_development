c2ce4b1a099bdc22972ac811c0be389a12003337
mehmood-zaman <mehmood497@gmail.com>
2021-07-28 10:53:02 +0500

    updated

--- TS/sorts/src/index.ts
+++ TS/sorts/src/index.ts
@@ -1,15 +1,29 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // const numbersCollection = new NumbersCollection([0, 3, -3, 5]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("Xaayb");
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection("Xaayb");
+// const sorter = new Sorter(charactersCollection);
+
+// sorter.sort();
+
+// console.log(charactersCollection.data);
+
+const linkList = new LinkedList();
+linkList.add(500);
+linkList.add(-10);
+linkList.add(-3);
+linkList.add(4);
+linkList.add(0);
+
+const sorter = new Sorter(linkList);
 
 sorter.sort();
 
-console.log(charactersCollection.data);
+linkList.print();
