56ea5b118319eb97ea25e4100bb7cd08f7779d40
mehmood-zaman <mehmood497@gmail.com>
2021-10-07 10:38:26 +0500

    updated

--- TS/web/src/index.ts
+++ TS/web/src/index.ts
@@ -3,5 +3,11 @@ console.log("Hi there!");
 
 const user = new User({ name: "Mehmood", age: 25 });
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+// user.set({name:"test"})
+
+// console.log(user.get("name"));
+// console.log(user.get("age"));
+
+user.on("change", () => {})
+
+console.log(user)
--- TS/web/src/models/User.ts
+++ TS/web/src/models/User.ts
@@ -1,11 +1,34 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
+type CallBack = () => void;
 export class User {
+  event: { [key: string]: CallBack[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
     return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: CallBack): void {
+    const handlers = this.event[eventName] || [];
+
+    handlers.push(callback);
+    this.event[eventName] = handlers;
+  }
+
+  trigger(eventName): void {
+    const handlers = this.event[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
