f13473606ddf8a8c30935b91033b6473ac38559f
mehmood-v <mehmood.zaman@venrup.com>
2022-05-30 06:15:43 +0500

    updated

--- Microfrontends/ecomm-boilerplate/container/webpack.config.js
+++ Microfrontends/ecomm-boilerplate/container/webpack.config.js
@@ -1,11 +1,15 @@
 const htmlWebpackPlugin = require("html-webpack-plugin");
-
+const ModuleFedrationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: {
     port: 8080,
   },
   plugins: [
+    new ModuleFedrationPlugin({
+      name: "container",
+      remotes: { products: "products@http://localhost:8081/remoteEntry.js" },
+    }),
     new htmlWebpackPlugin({
       template: "public/index.html",
     }),
--- Microfrontends/ecomm-boilerplate/products/webpack.config.js
+++ Microfrontends/ecomm-boilerplate/products/webpack.config.js
@@ -1,10 +1,18 @@
 const htmlWebpackServe = require("html-webpack-plugin");
+const ModuleFedrationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: {
     port: 8081,
   },
   plugins: [
+    new ModuleFedrationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
     new htmlWebpackServe({
       template: "public/index.html",
     }),
