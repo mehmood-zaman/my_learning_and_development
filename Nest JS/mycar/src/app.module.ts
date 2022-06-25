0be521517aeda8a13d7b0c08cad4aa5655162e61
mehmood-v <mehmood.zaman@venrup.com>
2022-06-25 11:36:39 +0500

    updated

---
+++ Nest JS/mycar/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import {TypeOrmModule} from '@type'
+
+@Module({
+  imports: [UsersModule, ReportsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
